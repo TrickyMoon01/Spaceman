7b4f6439379d19d01729fe45041ba4357859c19e
JamesYon <trickymoon01@gmail.com>
2025-07-11 23:20:47 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+Variables
+let word = ["Lebron James", "Michael Jordan", "Luka Doncic", "Cooper Flagg", "Kobe Bryant", "Anthony Davis", "Anthony Edwards", "Ben Simmons", "Ja Morant", "Kyrie Irving"]
+let board = [];
+let guess = [];
+let wrong = [];
+let right = [];
+
+// i need it to pick a random string from the array
+
+// if (guess === right) {
+//     console.log(guess);
+//     show the letter and display it
+// } else {
+//     show in the wrong board;
+// };
